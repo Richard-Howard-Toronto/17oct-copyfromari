85f90b992a7accfd6f0aaa68e81f9416ffb61406
Richard-Howard-Toronto <rh_gk@rogers.com>
2017-10-07 14:33:06 -0400

    updates crm exercise on sat oct 7 at 232 pm

--- crm.rb
+++ crm.rb
@@ -1,12 +1,8 @@
 require 'sinatra'
- require_relative 'contact'
+require_relative 'contact'
 
- get '/' do
+ get('/') do
    redirect to('/index')
- end
-
- get '/about' do
-     erb :about
  end
 
  get '/index' do
@@ -14,16 +10,37 @@ require 'sinatra'
    erb :index
  end
 
+ get '/about' do
+     erb :about
+ end
+
+ get '/photos' do  #ROUTES, the url
+   erb :photos   # you could call this anything
+ end
+
   get '/add_contact' do
     erb :add_contact
   end
 
+  get '/contacts/:id' do  #if we had kittens, params i.d.  would be kittens
+    # params[:id] contains the id from the URL
+    @contact = Contact.find_by({id: params[:id].to_i})
+    if @contact
+      erb :show_contact
+    else
+      raise Sinatra::NotFound
+    end
+
+  end
+
+
+
   post '/add_contact' do
     Contact.create(
-      first_name: params[:first_name],
-      last_name: params[:last_name],
+      first_name: params[:first_name],   #like "JANE" hash of info
+      last_name: params[:last_name],   #their names become the keys and what you type is becomes the value (of the has)
       email: params[:email],
-      note: params[:note]
+      note: params[:stuff_about_us]
     )
 
     redirect to('/index')
